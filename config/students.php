f222f9d1c3dd5b72f3843bde67af1d0e68199b96
Guy Roger Eboulet <eboulet87@gmail.com>
2020-03-18 08:21:41 +0100

    dopo il corso ok

--- config/students.php
+++ config/students.php
@@ -7,7 +7,8 @@ return [
       "eta" => "25",
       "azienda" => "DISC SPA",
       "ruolo" => "web developer",
-      "descrizione" => "Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer."
+      "descrizione" => "Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.",
+      "genere" => "m"
     ],
 
     [
@@ -16,7 +17,8 @@ return [
       "eta" => "24",
       "azienda" => "Prima Assicurazioni",
       "ruolo" => "junior software engineer",
-      "descrizione" => "A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer."
+      "descrizione" => "A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.",
+      "genere" => "f"
     ],
 
     [
@@ -25,7 +27,8 @@ return [
       "eta" => "36",
       "azienda" => "The Zen Agency",
       "ruolo" => "web developer",
-      "descrizione" => "Ha scoperto la passione per l'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web."
+      "descrizione" => "Ha scoperto la passione per l'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.",
+      "genere" => "f"
     ],
 
     [
@@ -34,7 +37,8 @@ return [
       "eta" => "29",
       "azienda" => "ArmadioVerde",
       "ruolo" => "web developer",
-      "descrizione" => "Dopo la laurea magistrale in scienze umanistiche e sociali, si concentra sullo sviluppo di strategie marketing. Decide di cambiare vita, questa volta puntando sullo sviluppo Front-End."
+      "descrizione" => "Dopo la laurea magistrale in scienze umanistiche e sociali, si concentra sullo sviluppo di strategie marketing. Decide di cambiare vita, questa volta puntando sullo sviluppo Front-End.",
+      "genere" => "m"
     ]
   ]
 ];
