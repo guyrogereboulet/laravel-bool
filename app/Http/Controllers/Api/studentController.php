56e382dbc67c203ac2b634cb72180849fd73d658
Guy Roger Eboulet <eboulet87@gmail.com>
2020-03-20 16:14:45 +0100

    Filtro nome e eta

--- app/Http/Controllers/Api/studentController.php
+++ app/Http/Controllers/Api/studentController.php
@@ -9,13 +9,22 @@ class studentController extends Controller
 {
     public function __construct()
     {
+     //Prendo l'array con il metodo config
      $this->students = config("students.students");
     }
 
     public function all()
     {
+     //Salvo l'array in una variabile $students
      $students = $this->students;
-     return response()->json($students);
+     //Creo un array vuoto
+     $studentAge = [];
+     //faccio un ciclo per filtrare nome e eta
+     foreach ($students as $student) {
+        $studentAge[$student['nome']] = $student['eta'];
+     }
+
+     return response()->json($studentAge);
     }
 
 }
